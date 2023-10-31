ee8f787ab7f4cd2b3c2c22245712a3a668db588a
CristopherH11 <cristo1206hc@gmail.com>
2023-10-30 19:15:55 -0600

    Add project files.

---
+++ BLOG/Models/Data/BlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BLOG.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BLOG.Models.Data;
+public class BlogDbContext : DbContext
+{
+    public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
+
---
+++ BLOG/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BLOG.Models
+{
+    public class Post
+    {
+        public Post() { Comments = new List<Comment>(); }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime PostDate { get; set; }
+
+        /*public string Author { get; set; }*/
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
