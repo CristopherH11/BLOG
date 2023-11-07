a0340723901f156f094d145990ced7c698a6526e
Michelle Brooke Fonseca Carrillo <michelle.fonsecacarrillo@ucr.ac.cr>
2023-11-07 12:15:12 -0600

    Removed unused imports

--- BLOG/Controllers/PostsController.cs
+++ BLOG/Controllers/PostsController.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BLOG.Models;
 using BLOG.Models.Data;
-using Microsoft.Extensions.Hosting;
 
 namespace BLOG.Controllers
 {
