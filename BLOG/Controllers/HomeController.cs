a2b64c0ccd1e0b647d6c255dd1de48f2bcbf4290
Michelle Brooke Fonseca Carrillo <michelle.fonsecacarrillo@ucr.ac.cr>
2023-11-07 20:51:45 -0600

    Formateando la vista

--- BLOG/Controllers/HomeController.cs
+++ BLOG/Controllers/HomeController.cs
@@ -1,21 +1,38 @@
 ﻿using BLOG.Models;
+using BLOG.Models.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Dynamic;
 
 namespace BLOG.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BlogDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BlogDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // Category
+            dynamic mymodel = new ExpandoObject();
+            mymodel.Categories = await _context.Categories.ToListAsync();
+
+            // Authors
+            var tempAuthors = from m in _context.Posts select m.Author;
+            mymodel.Authors = await tempAuthors.Distinct().ToListAsync();
+
+            // Posts
+            var tempPosts = from m in _context.Posts orderby m.PostDate descending select m;
+            mymodel.Posts = await tempPosts.ToListAsync();
+
+            return View(mymodel);
         }
 
         public IActionResult Privacy()
---
+++ BLOG/Utility/CustomHtmlHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+public static class CustomHtmlHelper
+{
+    public static IHtmlContent Truncate(this IHtmlHelper helper, string text, int maxLength)
+    {
+        if (text.Length <= maxLength)
+        {
+            return new HtmlString(text);
+        }
+
+        string truncatedText = text.Substring(0, maxLength);
+        return new HtmlString($"{truncatedText}...");
+    }
+}
+
